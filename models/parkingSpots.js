1e7d0fa64cb0dcd0c423833c30508091ae07d1ac
fouzilali <fouzilali@gmail.com>
2020-11-17 23:22:36 +0800

    updated error in LicencePlateSchema

--- models/parkingSpots.js
+++ models/parkingSpots.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const licensePlateSchema = new Schema({
+    licensePlate: {
+        type: String
+    },
     userProfile: {
-        type: String,
+        type: String
     }
 }, { timestamp: true });
 
