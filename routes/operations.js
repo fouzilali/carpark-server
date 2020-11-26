0159aa7f1f31a1ec4f4736de266e67632048f196
fouzilali <fouzilali@gmail.com>
2020-11-26 17:31:10 +0800

    added operation APIs

--- routes/operations.js
+++ routes/operations.js
@@ -3,21 +3,77 @@ var router = express.Router();
 
 // TODO: 'operation' stage API functions
 
-router.post('/spotFilled', function (req, res, next) {
+router.put('/spotFilled', function (req, res, next) {
     // spotFilled(CameraID, LPNumber, PixelLocation, time)
+    try{
+        result  = await  ParkingSpots.findOne({spotID : req.body.spotID}, function(err, doc){
+            doc.spotID = req.body.spotID;
+            doc.cameraID = req.body.cameraID;
+            doc.vacant = false;
+            doc.licensePlate = req.body.licensePlate;
+            doc.boundingBox = req.body.boundingBox;
+            doc.save();
+        }); 
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(result);
+    }catch(err){
+        console.error(err);
+        res.json(err);
+    }
 })
-router.post('/spotVacated', function (req, res, next) {
+router.put('/spotVacated', function (req, res, next) {
     // spotVacated(CameraID, PixelLocation, time)
+    try{
+        result  = await  ParkingSpots.findOne({spotID : req.body.spotID}, function(err, doc){
+            doc.spotID = req.body.spotID,
+            doc.cameraID = req.body.cameraID,
+            doc.vacant = true,
+            doc.licensePlate = null,
+            doc.boundingBox = null
+            doc.save();
+        }); 
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(result);
+    }catch(err){
+        console.error(err);
+        res.json(err)
+    }
 })
 
 router.get('/isFilled', function (req, res, next) {
-    // isFilled(ParkingSpot) -> bool
+    try{
+        result  = await  ParkingSpots.findOne({spotID: req.body.spotID}); 
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(!(result.vacant));
+    }catch(err){
+        console.error(err);
+        res.json(err);
+    }
 })
 router.get('/getLPNumber', function (req, res, next) {
-    // getLPNumber(ParkingSpot) -> licence plate
-})
+    try{
+        result  = await  ParkingSpots.findOne({spotID: req.body.spotID}); 
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(result.licensePlate);
+    }catch(err){
+        console.error(err);
+        res.json(err);
+    }})
 router.get('/isVacated', function (req, res, next) {
     // isVacated(ParkingSpot) -> bool
+    try{
+        result  = await  ParkingSpots.findOne({spotID: req.body.spotID}); 
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(result.vacant);
+    }catch(err){
+        console.error(err);
+        res.json(err);
+    }
 })
 router.get('/userProfile', function (req, res, next) {
     // userProfile(licensePlate)-> userProfile
