5f41d0141671d11975aeae84687b4858cbe87551
Anshuman <amedhi@connect.ust.hk>
2021-04-21 15:52:58 +0800

    tmux start everything

---
+++ tmux_start.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+session="servers"
+
+CARPARK_SERVER_DIR=${CARPARK_SERVER_DIR:-"."}
+DASH_SERVER_DIR=${DASH_SERVER_DIR:-"$CARPARK_SERVER_DIR/Dash/Source\ Files/"}
+LPR_SERVER_DIR=${LPR_SERVER_DIR:-"../fyp-lpr-gcp-functions"}
+
+tmux new-session -d -s $session
+
+tmux rename-window -t $session:0 "lpr"
+tmux new-window -t $session:1 -n 'back'
+tmux new-window -t $session:2 -n 'dash'
+tmux new-window -t $session:3 -n 'htop'
+
+tmux send-keys -t "lpr" 'bash' C-m 'clear' C-m 
+tmux send-keys -t "lpr" "cd ${LPR_SERVER_DIR}" C-m 
+tmux send-keys -t "lpr" 'source ./.venv/bin/activate' C-m
+tmux send-keys -t "lpr" 'python server.py' C-m
+
+tmux send-keys -t "back" 'bash' C-m 'clear' C-m 
+tmux send-keys -t "back" "cd ${CARPARK_SERVER_DIR}" C-m 
+tmux send-keys -t "back" 'npm run start' C-m 
+
+tmux send-keys -t "dash" 'bash' C-m 'clear' C-m 
+tmux send-keys -t "dash" "cd ${DASH_SERVER_DIR}" C-m 
+tmux send-keys -t "dash" 'npm run start' C-m 
+
+tmux send-keys -t "htop" 'bash' C-m 'clear' C-m 
+tmux send-keys -t "htop" "htop" C-m 
